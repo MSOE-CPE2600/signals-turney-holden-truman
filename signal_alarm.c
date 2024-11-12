3d82c16d9683121926f3d535d1d119d1198ae9d9
holden <trumanh@msoe.edu>
2024-11-12 16:19:17 -0600

    part 3

--- signal_alarm.c
+++ signal_alarm.c
@@ -1,6 +1,31 @@
 /**
- * File: 
- * Modified by:
+ * File: signal_alarm.c
+ * Modified by: Holden Truman
  * 
- * Brief summary of program:
+ * Brief summary of program: After program initializes it waits five seconds to receive an alarm, then
+ * prints that it received the alarm. The program doesn't close unless it is terminated/killed
+ * by the user.
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+// Signal handler for SIGALRM
+void handle_alarm(int sig) {
+    // Print when the alarm signal is received
+    printf("Signal received: SIGALRM\n");
+}
+
+int main() {
+    // Set up the signal handler for SIGALRM
+    signal(SIGALRM, handle_alarm);
+    // Schedule an alarm to go off in 5 seconds
+    alarm(5);
+    // Wait indefinitely, allowing the program to catch the signal
+    while(1) {
+        sleep(1);  // Keep the program open
+    }
+    return 0;
+}
--- signal_segfault.c
+++ signal_segfault.c
@@ -4,15 +4,26 @@
  */
 
 /**
- * Modified by:
+ * Modified by: Holden Truman
  * 
- * Brief summary of modifications:
+ * Brief summary of modifications: I added a line to handle the segmentation fault
+ * signal that occurs when dereferencing the NULL pointer. When this happens a statement is printed,
+ * then the program continues again, and tries to dereference the NULL pointer again, which causes a loop
+ * of segmentation fault signal to be received and handled.
  */
 
 
 #include <stdio.h>
+#include <signal.h>
+
+void handle_signal() {
+    printf("Segmentation Fault Received\n");
+}
 
 int main (int argc, char* argv[]) {
+    // Register for the signal
+    signal(SIGSEGV, handle_signal);
+
     // Declare a null pointer
     int* i = NULL;
 
